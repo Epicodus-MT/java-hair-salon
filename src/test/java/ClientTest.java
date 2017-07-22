b785770409ea127f4db39e1c5103fcd8db20358b
Mara Timberlake <maratimberlake@msn.com>
2017-07-22 12:42:48 -0700

    update StylistTest.java and ClientTest.java with new tests

--- src/test/java/ClientTest.java
+++ src/test/java/ClientTest.java
@@ -12,10 +12,54 @@ public class ClientTest {
 
   @Test
   public void client_instantiatesCorrectly_true() {
-    Client testClient = new Client("Mary", 1, 2);
+    Client testClient = new Client("Johnny", 1, 2);
     assertEquals(true, testClient instanceof Client);
   }
 
+  @Test
+  public void Client_instantiatesWithName_String() {
+    Client testClient = new Client("Johnny", 1, 2);
+    assertEquals("Johnny", testClient.getName());
+  }
 
+  @Test
+  public void equals_returnsTrueIfNameAndStylistIdAreSame_true() {
+    Client testClient = new Client("Johnny", 1, 2);
+    Client anotherClient = new Client("Johnny", 1, 2);
+    assertTrue(testClient.equals(anotherClient));
+  }
 
+  @Test
+  public void save_successfullyAddsClientToDatabase_List() {
+    Client testClient = new Client("Johnny", 1, 2);
+    testClient.save();
+    assertTrue(Client.all().get(0).equals(testClient));
+  }
+
+  @Test
+  public void save_assignsIdToClient() {
+    Client testClient = new Client("Johnny", 1, 2);
+    testClient.save();
+    Client savedClient = Client.all().get(0);
+    assertEquals(savedClient.getId(), testClient.getId());
+  }
+
+  @Test
+  public void all_returnsAllInstancesOfClient_true() {
+    Client firstClient = new Client("Johnny", 1, 2);
+    firstClient.save();
+    Client secondClient = new Client("Ben", 3, 4);
+    secondClient.save();
+    assertEquals(true, Client.all().get(0).equals(firstClient));
+    assertEquals(true, Client.all().get(1).equals(secondClient));
+  }
+
+  @Test
+  public void find_returnsClientWithSameId_secondClient() {
+    Client firstClient = new Client("Johnny", 1, 2);
+    firstClient.save();
+    Client secondClient = new Client("Ben", 3, 4);
+    secondClient.save();
+    assertEquals(Client.find(secondClient.getId()), secondClient);
+  }
 }
--- src/test/java/StylistTest.java
+++ src/test/java/StylistTest.java
@@ -10,15 +10,63 @@ public class StylistTest {
 
   @Test
   public void stylist_instantiatesCorrectly_true() {
-    Stylist testStylist = new Stylist("Johnny");
+    Stylist testStylist = new Stylist("Johnny", "Jono@HamiltonRadio.com");
     assertEquals(true, testStylist instanceof Stylist);
   }
 
+  @Test
+  public void getName_stylistInstantiatesWithName_Kelle() {
+    Stylist testStylist = new Stylist("Johnny", "Jono@HamiltonRadio.com");
+    assertEquals("Johnny", testStylist.getName());
+  }
 
+  @Test
+  public void getEmail_StylistInstantiatesWithEmail_String() {
+    Stylist testStylist = new Stylist("Johnny", "Jono@HamiltonRadio.com");
+    assertEquals("Jono@HamiltonRadio.com", testStylist.getEmail());
+  }
 
+  @Test
+  public void save_insertsObjectIntoDatabase_Stylist() {
+    Stylist testStylist = new Stylist("Johnny", "Jono@HamiltonRadio.com");
+    testStylist.save();
+    assertEquals(true, Stylist.all().get(0).equals(testStylist));
+  }
 
+  @Test
+  public void save_assignsIdToObject() {
+    Stylist testStylist = new Stylist("Johnny", "Jono@HamiltonRadio.com");
+    testStylist.save();
+    Stylist savedStylist = Stylist.all().get(0);
+    assertEquals(testStylist.getId(), savedStylist.getId());
+  }
 
+  @Test
+  public void equals_returnsTrueIfNameAndEmailAreSame_true() {
+    Stylist testStylist = new Stylist("Johnny", "Jono@HamiltonRadio.com");
+    Stylist anotherStylist = new Stylist("Johnny", "Ben@HamiltonRadio.com");
+    assertTrue(testStylist.equals(anotherStylist));
+  }
 
+  @Test
+  public void all_returnsAllInstancesOfStylist_true() {
+    Stylist firstStylist = new Stylist("Johnny", "Jono@HamiltonRadio.com");
+    firstStylist.save();
+    Stylist secondStylist = new Stylist("Benjamin", "Ben@HamiltonRadio.com");
+    secondStylist.save();
+    assertEquals(true, Stylist.all().get(0).equals(firstStylist));
+    assertEquals(true, Stylist.all().get(1).equals(secondStylist));
+  }
 
-  
+  @Test
+  public void getClients_retrievesAllClientsFromDatabase_clientsList() {
+    Stylist testStylist = new Stylist("Johnny", "Jono@HamiltonRadio.com");
+    testStylist.save();
+    Client firstClient = new Client("Izzy", testStylist.getId());
+    firstClient.save();
+    Client secondClient = new Client("Thomas", testStylist.getId());
+    secondClient.save();
+    Object[] clients = new Object[] { firstClient, secondClient };
+    assertTrue(testStylist.getClients().containsAll(Arrays.asList(clients)));
+  }
 }
