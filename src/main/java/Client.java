a6f8a0f0e769e25c5e94b61f8000ff3e6ff05e0b
Mara Timberlake <maratimberlake@msn.com>
2017-07-22 16:57:36 -0700

    update src/main/java files with full app details

--- src/main/java/Client.java
+++ src/main/java/Client.java
@@ -2,15 +2,15 @@ import java.util.ArrayList;
 import java.util.List;
 import org.sql2o.*;
 
-public class Client {
+public class Product {
   private String name;
-  private String note;
+  private String detail;
   private int stylistId;
   private int id;
 
-  public Client(String name, String note, int stylistId) {
+  public Product(String name, String detail, int stylistId) {
     this.name = name;
-    this.note = note;
+    this.detail = detail;
     this.stylistId = stylistId;
   }
 
@@ -18,8 +18,8 @@ public class Client {
     return name;
   }
 
-  public String getNote() {
-    return note;
+  public String getDetail() {
+    return detail;
   }
 
   public int getId() {
@@ -30,5 +30,108 @@ public class Client {
     return stylistId;
   }
 
+
+  @Override
+  public boolean equals(Object otherProduct){
+    if (!(otherProduct instanceof Product)) {
+      return false;
+    } else {
+      Product newProduct = (Product) otherProduct;
+      return this.getName().equals(newProduct.getName()) &&
+             this.getDetail()==(newProduct.getDetail());
+    }
+  }
+
+  public void save() {
+    try(Connection con = DB.sql2o.open()) {
+      String sql = "INSERT INTO clients (name, detail, stylistId) VALUES (:name, :detail, :stylistId)";
+      this.id = (int) con.createQuery(sql, true)
+        .addParameter("name", this.name)
+        .addParameter("detail", this.detail)
+        .addParameter("stylistId", this.stylistId)
+        .executeUpdate()
+        .getKey();
+    }
+  }
+
+  public static List<Product> all() {
+    String sql = "SELECT id, name, detail FROM clients";
+    try(Connection con = DB.sql2o.open()) {
+     return con.createQuery(sql).executeAndFetch(Product.class);
+    }
+  }
+
+  public List<Object> getProducts() {
+    List<Object> allProducts = new ArrayList<Object>();
+
+    try(Connection con = DB.sql2o.open()) {
+      String sql = "SELECT * FROM clients WHERE clientId=:id;";
+      List<Product> clients = con.createQuery(sql)
+        .addParameter("id", this.id)
+        .throwOnMappingFailure(false)
+        .executeAndFetch(Product.class);
+      allProducts.addAll(clients);
+    }
+    return allProducts;
+  }
+
+  public List<Stylist> getStylists() {
+    try(Connection con = DB.sql2o.open()){
+      String joinQuery = "SELECT stylist_id FROM stylists_clients WHERE client_id = :client_id";
+      List<Integer> stylistIds = con.createQuery(joinQuery)
+        .addParameter("client_id", this.getId())
+        .executeAndFetch(Integer.class);
+      List<Stylist> stylists = new ArrayList<Stylist>();
+      for (Integer stylistId : stylistIds) {
+        String stylistQuery = "SELECT * FROM stylists WHERE id = :stylistId";
+        Stylist stylist = con.createQuery(stylistQuery)
+          .addParameter("stylistId", stylistId)
+          .executeAndFetchFirst(Stylist.class);
+        stylists.add(stylist);
+      }
+      return stylists;
+    }
+  }
+
+  public void leaveStylist(Stylist stylist){
+    try(Connection con = DB.sql2o.open()){
+      String joinRemovalQuery = "DELETE FROM stylists_clients WHERE stylist_id = :stylistId AND client_id = :clientId;";
+      con.createQuery(joinRemovalQuery)
+        .addParameter("stylistId", stylist.getId())
+        .addParameter("clientId", this.getId())
+        .executeUpdate();
+    }
+  }
+
+  public static Product find(int id) {
+    try(Connection con = DB.sql2o.open()) {
+      String sql = "SELECT * FROM clients where id=:id";
+      Product client = con.createQuery(sql)
+        .addParameter("id", id)
+        .throwOnMappingFailure(false)
+        .executeAndFetchFirst(Product.class);
+    return client;
+    }
+  }
+
+  public void delete() {
+    try(Connection con = DB.sql2o.open()) {
+    String sql = "DELETE FROM clients WHERE id = :id;";
+    con.createQuery(sql)
+      .addParameter("id", id)
+      .executeUpdate();
+    }
+  }
+
+  public void update(String name, String detail) {
+    try(Connection con = DB.sql2o.open()) {
+      String sql = "UPDATE clients SET name = :name, detail = :detail WHERE id = :id";
+      con.createQuery(sql)
+        .addParameter("name", name)
+        .addParameter("detail", detail)
+        .addParameter("id", id)
+        .executeUpdate();
+    }
+  }
 
 }
--- src/main/java/Stylist.java
+++ src/main/java/Stylist.java
@@ -1,23 +1,134 @@
-import org.sql2o.*;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.*;
+import java.util.HashMap;
+import java.util.Map;
+import spark.ModelAndView;
+import spark.template.velocity.VelocityTemplateEngine;
+import static spark.Spark.*;
 
-public class Stylist {
-  private String name;
-  private int id;
+public class App {
+  public static void main(String[] args) {
+    staticFileLocation("/public");
+    String layout = "templates/layout.vtl";
 
-  public Stylist(String name) {
-    this.name = name;
+    get("/", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      model.put("stylists", Stylist.all());
+      model.put("template", "templates/index.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    get("clients/new", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      model.put("template", "templates/client-form.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    get("/clients", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      model.put("clients", Product.all());
+      model.put("template", "templates/clients.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    post("/clients", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      Stylist stylist = Stylist.find(Integer.parseInt(request.queryParams("stylistId")));
+      String name = request.queryParams("name");
+      String detail = request.queryParams("detail");
+      Product newProduct = new Product(name, detail, stylist.getId());
+      newProduct.save();
+      model.put("stylist", stylist);
+      model.put("template", "templates/stylist-client-success.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    get("/clients/:id", (request, response) -> {
+      HashMap<String, Object> model = new HashMap<String, Object>();
+      Product client = Product.find(Integer.parseInt(request.params(":id")));
+      Stylist stylist = Stylist.find(client.getStylistId());
+      model.put("stylist", stylist);
+      model.put("client", client);
+      model.put("template", "templates/client.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    get("/stylists/new", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      model.put("template", "templates/stylist-form.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    post("/stylists", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      String name = request.queryParams("name");
+      Stylist newStylist = new Stylist(name);
+      newStylist.save(); // Saves to the stylist database table
+      model.put("template", "templates/stylist-success.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    get("/stylists", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      model.put("stylists", Stylist.all());
+      model.put("template", "templates/stylists.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    get("stylists/:id/clients/new", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      Stylist stylist = Stylist.find(Integer.parseInt(request.params(":id")));
+      model.put("stylist", stylist);
+      model.put("template", "templates/stylist-client-form.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    get("/stylists/:id", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      Stylist stylist = Stylist.find(Integer.parseInt(request.params(":id")));
+      model.put("clients", Product.all());
+      model.put("stylist", stylist);
+      model.put("template", "templates/stylist.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    get("/stylists/:stylist_id/clients/:id", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      Stylist stylist = Stylist.find(Integer.parseInt(request.params(":stylist_id")));
+      Product client = Product.find(Integer.parseInt(request.params(":id")));
+      model.put("stylist", stylist);
+      model.put("client", client);
+      model.put("template", "templates/client.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    post("/stylists/:stylist_id/clients/:id", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      Product client = Product.find(Integer.parseInt(request.params("id")));
+      String name = request.queryParams("name");
+      String detail = request.queryParams("detail");
+      Stylist stylist = Stylist.find(client.getStylistId());
+      client.update(name, detail);
+      String url = String.format("/stylists/%d/clients/%d", stylist.getId(), client.getId());
+      response.redirect("/");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    post("/stylists/:stylist_id/clients/:id/delete", (request, response) -> {
+      HashMap<String, Object> model = new HashMap<String, Object>();
+      Product client = Product.find(Integer.parseInt(request.params("id")));
+      Stylist stylist = Stylist.find(client.getStylistId());
+      client.delete();
+      model.put("stylist", stylist);
+      model.put("template", "templates/delete-success.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    post("/stylists/:id/delete", (request, response) -> {
+      HashMap<String, Object> model = new HashMap<String, Object>();
+      Stylist stylist = Stylist.find(Integer.parseInt(request.params(":id")));
+      String deletedStylistName = stylist.getName();
+      stylist.delete();
+      response.redirect("/");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
   }
-
-  public String getName() {
-    return name;
-  }
-
-  public int getId() {
-    return id;
-  }
-
-
 }
